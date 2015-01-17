bed45ee98a7788767371641b036f1872ef36ad77
RachelNolan <mail@rachelnolan.com>
2015-01-17 10:48:21 +0000

    Questions 1-2 completed

---
+++ frontEndTest.js
@@ -0,0 +1,96 @@
+Frontend Developer Test
+========================================================================
+===== */
+var yourName = 'Rachel Nolan',
+dateCompleted = '18/01/2015';
+//
+========================================================================
+=====
+// 1) Define the function 'camelCase', which takes a string parameter
+// for example 'your soul is mine', and returns 'yourSoulIsMine'
+//
+
+function camelCase(str) {
+  var shortStr = str.replace(/(^[\s]+|[\s]+$)/g, '')
+  return shortStr.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+  }).replace(/\s+/g, '');
+}
+
+ camelCase('your soul is mine');
+ // -> 'yourSoulIsMine'
+ camelCase(' Get over here! ');
+ // -> 'getOverHere!'
+
+========================================================================
+=====
+//
+========================================================================
+=====
+// 2) Place the 'camelCase' function directly on the 'String' object so that:
+//
+
+String.prototype.camelCase = function () {
+  var shortStr = this.replace(/(^[\s]+|[\s]+$)/g, '')
+  return shortStr.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+  }).replace(/\s+/g, '');
+}
+
+ 'your soul is mine'.camelCase()
+ // -> 'yourSoulIsMine'
+ ' Get over here! '.camelCase()
+ // -> 'getOverHere!'
+//
+========================================================================
+=====
+// 3) The function 'masterSum' below can take any number of 'Integer' arguments
+// and return the sum:
+ function masterSum () {
+ var result = 0;
+ for (var i = 0; i < arguments.length; i++) {
+ result += arguments[i];
+ }
+ return result;
+ }
+// Define a new function 'sum' that wraps around 'masterSum' and passes its
+// arguments on to 'masterSum', so that
+ sum(1,2,3,4,5,6);
+ // -> 21
+//
+
+========================================================================
+=====
+// 4) Rewrite the 'masterSum' function so it uses Array.prototype.reduce()
+//
+
+========================================================================
+=====
+// 5) Given the following object what will the output logs be
+//
+========================================================================
+=====
+ var Hero = {
+ hitPoints: 100,
+ getLife: function () {
+ return this.hitPoints;
+ }
+ };
+ console.log(Hero.getLife());
+ // a) -> ............
+ var LinksLife = Hero.getLife;
+ console.log(LinksLife());
+ // b) -> ............
+//
+
+========================================================================
+=====
+// 6) 5b is incorrect, use Function.prototype.bind to make it return the same
+// result as 5a:
+// console.log(Hero.getLife());
+//
+========================================================================
+=====
+//
+========
+
